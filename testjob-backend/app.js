39af35c1692a64152906475e78e544367f9c28cf
sharad_mourya <sharadmourya27@gmail.com>
2023-02-08 13:21:12 +0530

    Try Build 3

--- testjob-backend/app.js
+++ testjob-backend/app.js
@@ -5,10 +5,23 @@ const bodyParser = require("body-parser");
 const app = express();
 const usersRouter = require("./routes/usersRoute")
 const filesRouter = require("./routes/filesRoute");
-require("./config/database").connect();
+const mongoose = require('mongoose')
+const { DB_URI } = process.env;
+console.log("database connection string", DB_URI);
+// require("./config/database").connect();
 
 const port = process.env.PORT || 4000;
-
+mongoose
+    .connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to database sucessfully");
+    })
+    .catch((error) => {
+      console.log("error in database connection", error);
+    });
 
 app.use(cors());
 // for parsing application/json
